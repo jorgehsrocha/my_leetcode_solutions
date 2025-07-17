2f2b06f3c945ac892e7b069c27b63c876db1258a
Jorge Henrique de Sousa Rocha <jorge.henrique@duotalk.com.br>
2025-07-16 22:50:55 -0300

    add solution for 70. Climbing Stairs problem

---
+++ 70.climbing-stairs.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=70 lang=typescript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+    const BASE_VALUE_OF_STAIR = 1;
+    let memo: {[key: number]: number} = {};
+    
+    function waysToClimb(n: number, memo:{}) {
+        if (n<=1) return BASE_VALUE_OF_STAIR;
+        if (memo[n]) return memo[n];
+     
+        memo[n] = waysToClimb(n-1, memo) + waysToClimb(n-2, memo)
+        return memo[n];
+    }
+    if (n<=BASE_VALUE_OF_STAIR) return BASE_VALUE_OF_STAIR;
+    
+    return waysToClimb(n, memo);
+};
+// @lc code=end
+
